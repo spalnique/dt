36b94c8cb4319b3447ab008a806eaced8d31026f
spalnique <lex.sydoruk@gmail.com>
2025-05-01 23:17:17 +0200

    clean up 2

--- frontend/.next/static/chunks/src_app_page_tsx_0c057fc2._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_eeefdadc._.js",
-  "static/chunks/src_components_cbf38235._.js"
-],
-    source: "dynamic"
-});
